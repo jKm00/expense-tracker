dadc62faebe089ec18f4a908927aa3adeabd390e
Joakim Edvardsen <77448888+jKm00@users.noreply.github.com>
2026-03-17 20:42:01 +0100

    init: better auth with drizzle

--- src/lib/better-auth/auth.js
+++ src/lib/better-auth/auth.js
@@ -1,9 +1,18 @@
 import { betterAuth } from "better-auth";
 import { tanstackStartCookies } from "better-auth/tanstack-start";
+import { env } from "@/config/env";
+import { drizzleAdapter } from "@better-auth/drizzle-adapter";
+import { db } from "@/lib/db";
 
 export const auth = betterAuth({
-  emailAndPassword: {
-    enabled: true,
+  socialProviders: {
+    github: {
+      clientId: env.GITHUB_CLIENT_ID,
+      clientSecret: env.GITHUB_CLIENT_SECRET,
+    },
   },
+  database: drizzleAdapter(db, {
+    provider: "pg",
+  }),
   plugins: [tanstackStartCookies()],
 });
