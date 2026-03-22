5a4b79e02532a0793ded99b1cbab55e8537a2abb
Joakim Edvardsen <77448888+jKm00@users.noreply.github.com>
2026-03-22 23:33:14 +0100

    feat(products): add edit product form component

---
+++ src/features/products/components/edit-product.form.tsx
@@ -0,0 +1,83 @@
+import { ProductWithTags } from "../product.models";
+import { useForm } from "@tanstack/react-form-start";
+import { productValidators } from "../product.validators";
+import { productMutations } from "../product.mutations";
+import { Input } from "@/components/ui/input";
+import FieldError from "@/components/custom/field-error";
+import { LoaderButton } from "@/components/custom/loader.button";
+import { toast } from "sonner";
+
+export function EditProductForm({ product }: { product: ProductWithTags }) {
+  const mutation = productMutations.updateProduct();
+
+  const form = useForm({
+    defaultValues: {
+      name: product.name,
+    },
+    validators: {
+      onBlur: productValidators.editFormValidation,
+    },
+    onSubmit: ({ value }) => {
+      mutation.mutate(
+        {
+          productId: product.id,
+          name: value.name,
+        },
+        {
+          onSuccess: (data) => {
+            const [err] = data;
+            if (err) {
+              const errorMsg =
+                "message" in err ? err.message : "error" in err ? err.error : "Failed to update product";
+              toast.error(errorMsg);
+            } else {
+              toast.success("Product updated");
+            }
+          },
+        },
+      );
+    },
+  });
+
+  return (
+    <form
+      onSubmit={(e) => {
+        e.preventDefault();
+        form.handleSubmit();
+      }}
+    >
+      <form.Field
+        name="name"
+        children={(field) => (
+          <>
+            <label>Product Name</label>
+            <Input
+              name={field.name}
+              type="text"
+              value={field.state.value}
+              onBlur={field.handleBlur}
+              onChange={(e) => field.handleChange(e.target.value)}
+            />
+            <FieldError field={field} />
+          </>
+        )}
+      />
+      <form.Subscribe
+        selector={(state) => [
+          state.canSubmit,
+          state.isSubmitting,
+          state.isDefaultValue,
+        ]}
+        children={([canSubmit, isSubmitting, isDefaultValue]) => (
+          <LoaderButton
+            type="submit"
+            disabled={!canSubmit || isDefaultValue || mutation.isPending}
+            isLoading={mutation.isPending}
+          >
+            {isSubmitting ? "..." : "Save"}
+          </LoaderButton>
+        )}
+      />
+    </form>
+  );
+}
