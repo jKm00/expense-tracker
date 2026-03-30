5ac2660ba7e41be350aa88c71a5372db9dbff025
jKm00 <joakimedvardsen2000@gmail.com>
2026-03-30 13:34:46 +0200

    wip: month navigation

--- src/components/custom/page-header.tsx
+++ src/components/custom/page-header.tsx
@@ -1,14 +1,17 @@
 export function PageHeader({
   title,
   action,
+  children,
 }: {
   title: string;
   action?: React.ReactNode;
+  children?: React.ReactNode;
 }) {
   return (
     <div className="flex items-center justify-between">
       <h1 className="text-2xl font-semibold tracking-tight">{title}</h1>
       {action}
+      {children}
     </div>
   );
 }
--- src/routes/_app.dashboard.transactions.index.tsx
+++ src/routes/_app.dashboard.transactions.index.tsx
@@ -3,10 +3,13 @@ import { TransactionList } from "@/features/transactions/components/transaction-
 import { PageHeader } from "@/components/custom/page-header";
 import { SkeletonList } from "@/components/custom/skeleton-list";
 import { useSuspenseQuery } from "@tanstack/react-query";
-import { createFileRoute } from "@tanstack/react-router";
+import { createFileRoute, useNavigate } from "@tanstack/react-router";
 import { Suspense } from "react";
 import z from "zod";
 import { zodValidator } from "@tanstack/zod-adapter";
+import { Calendar, ChevronLeft, ChevronRight } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import dayjs from "dayjs";
 
 const dashboardSearchSchema = z.object({
   month: z.number().optional(),
@@ -25,9 +28,54 @@ export const Route = createFileRoute("/_app/dashboard/transactions/")({
 });
 
 function RouteComponent() {
+  const { month, year } = Route.useSearch();
+  const date = month && year ? new Date(year, month, 1) : new Date();
+  const navigate = useNavigate();
+
+  function prevMonth() {
+    const newDate = dayjs(date).subtract(1, "month");
+    console.log(newDate);
+    navigate({
+      to: "/dashboard/transactions",
+      search: {
+        month: newDate.get("month"),
+        year: newDate.get("year"),
+      },
+    });
+  }
+
+  function nextMonth() {
+    const newDate = dayjs(date).add(1, "month");
+    navigate({
+      to: "/dashboard/transactions",
+      search: {
+        month: newDate.get("month"),
+        year: newDate.get("year"),
+      },
+    });
+  }
+
   return (
     <div className="space-y-6">
-      <PageHeader title="Transactions" />
+      <PageHeader title="Transactions">
+        <div className="flex items-center gap-1">
+          <Button variant="outline" onClick={prevMonth}>
+            <ChevronLeft />
+          </Button>
+          {/*<div className="flex items-center gap-2 py-2 px-4 border rounded-md">*/}
+          <div className="flex gap-2 items-center border rounded-lg h-8 px-2.5 py-1 min-w-40">
+            <Calendar className="size-4" />
+            <p className="text-sm">
+              {date.toLocaleString("en-UK", {
+                month: "long",
+              })}
+            </p>
+          </div>
+          <Button variant="outline" onClick={nextMonth}>
+            <ChevronRight />
+          </Button>
+        </div>
+      </PageHeader>
       <Suspense fallback={<SkeletonList rows={8} />}>
         <TransactionsContent />
       </Suspense>
