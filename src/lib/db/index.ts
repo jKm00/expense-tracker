4948337a9a25b629a54a34526f991847060aae1b
Joakim Edvardsen <77448888+jKm00@users.noreply.github.com>
2026-04-01 15:57:56 +0200

    fix: imports after clean up

--- src/lib/db/index.ts
+++ src/lib/db/index.ts
@@ -1,6 +1,5 @@
 import { drizzle } from "drizzle-orm/node-postgres";
 
 import * as schema from "./schema";
-import { relations } from "./relations";
 
-export const db = drizzle(process.env.DATABASE_URL!, { schema, relations });
+export const db = drizzle(process.env.DATABASE_URL!, { schema });
--- src/lib/db/schema.ts
+++ src/lib/db/schema.ts
@@ -1,5 +1,1 @@
 export * from "@/features/auth/auth.schema";
-export * from "@/features/products/product.schema";
-export * from "@/features/recurring/recurring.schema";
-export * from "@/features/tags/tag.schema";
-export * from "@/features/transactions/transaction.schema";
