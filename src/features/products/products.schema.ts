8608e3d0b5eee9ce2dee2580f26c1ca2aaa2da36
Joakim Edvardsen <77448888+jKm00@users.noreply.github.com>
2026-04-01 16:44:26 +0200

    init: db schema w/ relations

---
+++ src/features/products/products.schema.ts
@@ -0,0 +1,32 @@
+import {
+  pgTable,
+  primaryKey,
+  text,
+  timestamp,
+  uuid,
+} from "drizzle-orm/pg-core";
+import { user } from "../auth/auth.schema";
+import { tags } from "../tags/tags.schema";
+
+export const products = pgTable("products", {
+  id: uuid("id").primaryKey().defaultRandom(),
+  userId: text("user_id")
+    .notNull()
+    .references(() => user.id, { onDelete: "cascade" }),
+  name: text("name").notNull(),
+  createdAt: timestamp("created_at").notNull().defaultNow(),
+  updatedAt: timestamp("updated_at").notNull().defaultNow(),
+});
+
+export const productTags = pgTable(
+  "product_tags",
+  {
+    productId: uuid("product_id")
+      .notNull()
+      .references(() => products.id, { onDelete: "cascade" }),
+    tagId: uuid("tag_id")
+      .notNull()
+      .references(() => tags.id, { onDelete: "restrict" }),
+  },
+  (table) => [primaryKey({ columns: [table.productId, table.tagId] })],
+);
