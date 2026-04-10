c770ebb22eae2341d4fc138306f6cf856ef3bfc2
Joakim Edvardsen <77448888+jKm00@users.noreply.github.com>
2026-04-10 22:15:58 +0200

    fix: migration with not null column

--- drizzle/20260407182430_careless_marvex/migration.sql
+++ drizzle/20260407182430_careless_marvex/migration.sql
@@ -1,1 +1,8 @@
-ALTER TABLE "recurring" ADD COLUMN "type" "entry_type" NOT NULL;
+-- Add the column as nullable so we can backfill existing rows
+ALTER TABLE "recurring" ADD COLUMN "type" "entry_type";
+
+-- Backfill existing rows with the default value 'expense'
+UPDATE "recurring" SET "type" = 'expense' WHERE "type" IS NULL;
+
+-- Make the column NOT NULL now that existing rows have values
+ALTER TABLE "recurring" ALTER COLUMN "type" SET NOT NULL;
