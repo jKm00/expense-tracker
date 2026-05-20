08444559f11f3a5a8b2c76a77807123e8b277fd2
Joakim Edvardsen <77448888+jKm00@users.noreply.github.com>
2026-05-20 23:02:30 +0200

    refactor: cron job to be handled by web app

--- apps/job/src/db/schema.ts
+++
@@ -1,5 +0,0 @@
-export * from "./schemas/auth.schema.js";
-export * from "./schemas/products.schema.js";
-export * from "./schemas/recurring.schema.js";
-export * from "./schemas/tags.schema.js";
-export * from "./schemas/transactions.schema.js";
